618667e6917be70f34a3705ab7c86bc82e757dcb
wsaadeh <wilson_saadeh@hotmail.com>
2024-10-14 14:44:03 -0300

    Final Commit

--- app/src/main/java/com/saadeh/artistlist/MainActivity.kt
+++ app/src/main/java/com/saadeh/artistlist/MainActivity.kt
@@ -4,13 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.saadeh.artistlist.ui.theme.ArtistListTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +42,132 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ArtistListTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                //Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                /*Greeting(
+                    name = "Android",
+                    modifier = Modifier.padding(innerPadding)
+                )*/
+                val leonardo = Artist(
+                    name = "Leonardo da Vinci",
+                    lastSeenOnline = "3 minutes ago",
+                    image = R.drawable.ic_leonardo_da_vinci,
+                    art = R.drawable.ic_mona_lisa
+                )
+
+                val picasso = Artist(
+                    name = "Pablo Picasso",
+                    lastSeenOnline = "5 minutes ago",
+                    image = R.drawable.ic_pablo_picasso,
+                    art = R.drawable.ic_beijo
+                )
+
+                val salvador = Artist(
+                    name = "Salvador Dali",
+                    lastSeenOnline = "7 minutes ago",
+                    image = R.drawable.ic_salvador_dali,
+                    art = R.drawable.ic_persistence_of_memory
+                )
+
+                val vanGogh = Artist(
+                    name = "Vincent Vangouh",
+                    lastSeenOnline = "10 minutes ago",
+                    image = R.drawable.ic_vincent_van_gogh,
+                    art = R.drawable.ic_starry_night
+                )
+
+                val artists = listOf(vanGogh, salvador, picasso, leonardo)
+
+                LazyColumn {
+                    items(artists) { artist ->
+                        ArtistCard(
+                            artist = artist,
+                            onClick = {
+                                println("Wilson test " + artist.name)
+                            }
+                        )
+                    }
+
                 }
+
+
+                //}
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ArtistCard(
+    artist: Artist,
+    onClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable(onClick = onClick)
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.FillWidth,
+                painter = painterResource(id = artist.image),
+                contentDescription = "Artist image"
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Column() {
+                Text(
+                    text = artist.name,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+                Text(
+                    text = artist.lastSeenOnline,
+                    color = Color.Gray
+                )
+            }
+        }
+        Card(
+            modifier = Modifier
+                .padding(8.dp),
+            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+        ) {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop,
+                painter = painterResource(id = artist.art),
+                contentDescription = "Artist Art"
+            )
+        }
+    }
 }
+
+data class Artist(
+    val name: String,
+    val lastSeenOnline: String,
+    @DrawableRes val image: Int,
+    @DrawableRes val art: Int,
+)
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ArtistCardPreview() {
     ArtistListTheme {
-        Greeting("Android")
+        val artist = Artist(
+            name = "Leonardo da Vinci",
+            lastSeenOnline = "3 minutes ago",
+            image = R.drawable.ic_leonardo_da_vinci,
+            art = R.drawable.ic_mona_lisa
+        )
+
+        ArtistCard(
+            artist,
+            onClick = {
+
+            })
     }
 }
+
